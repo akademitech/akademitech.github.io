3b92a184aaea293cd8e17755f2be778f809f6ee7
akademitech <ffayz4323@gmail.com>
2026-08-10 15:15:54 +0700

    Delete fix_array.py

--- fix_array.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open('generate_tutorials.py', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-# Instead of parsing python, I can just modify the text
-# Or simply use python to parse the file, move the block, and save.
